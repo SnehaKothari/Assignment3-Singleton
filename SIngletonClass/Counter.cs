e139311c23a0dac3e5a9bd8fe44b16fe01137a2e
Sneha <kothari.sneha05@gmail.com>
2023-01-05 23:57:13 +0800

    SIngleton class

---
+++ SIngletonClass/Counter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIngletonClass
+{
+    public sealed class Counter
+    {
+        private static int count1 = 0;
+        static int count = 0;
+        private static Counter instance = null;
+        private Counter()
+        {
+            count1++;
+            Console.WriteLine("Value: "+count1);
+        }
+       public static Counter getInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Counter();
+                return instance;
+            }
+        }
+        public void increment()
+        {
+            count = count + 1;
+            Console.WriteLine("Increment value:" + count);
+        }
+        public void decrement()
+        {
+            count = count - 1;
+            Console.WriteLine("Decrement value:" + count);
+        }
+        public void Currentcounter()
+        {
+            Console.WriteLine("Current count is: " + count);
+        }
+    }
+}
---
+++ SIngletonClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using SIngletonClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SingletonClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter count = Counter.getInstance;
+            count.increment();
+            count.increment();
+            count.decrement();
+           
+            count.Currentcounter();
+            
+        }
+    }
+}
+
